232ac6653b35b8133f3b70d0ed327e7190d07f9c
LGXerxes <ditisluca@yahoo.it>
2025-07-10 15:24:22 +0200

    need to redo tests, numbers might be incurrect

--- src/tests/test.rs
+++ src/tests/test.rs
@@ -1,11 +1,8 @@
 use std::{io::Write, os::unix::fs::MetadataExt};
 
-const CHUNKSIZE: usize = 100;
-// const CHUNKSIZE: usize = 1_024;
-const CHUNKDATA_SIZE: usize = CHUNKSIZE * CHUNKSIZE / 2;
-const CHUNKINDEX_SIZE: usize = CHUNKSIZE / 2;
+use paintplayground::types::{CHUNK_BYTE_SIZE, CHUNK_LENGTH, CHUNK_SIZE, Chunk};
 
-const CHUNKS_IN_DIRECTION: usize = 40000;
+const CHUNKS_IN_DIRECTION: usize = 20000;
 const CHUNKS_IN_MAP: usize = CHUNKS_IN_DIRECTION * CHUNKS_IN_DIRECTION;
 
 // TODO:
@@ -16,19 +13,12 @@ const CHUNKS_IN_MAP: usize = CHUNKS_IN_DIRECTION * CHUNKS_IN_DIRECTION;
 
 #[test]
 fn calculate_stuff() {
-    // maximun value of u21
-    let num: usize = (1 << 13) - 1;
-    println!("u13: {}", num);
-
-    // let CHUNKSIZE = 1_024u128;
-
-    println!("u20: {}", CHUNKS_IN_DIRECTION);
     println!(
         "playable area: {}x{}",
-        CHUNKS_IN_DIRECTION * CHUNKSIZE,
-        CHUNKS_IN_DIRECTION * CHUNKSIZE
+        CHUNKS_IN_DIRECTION * CHUNK_SIZE,
+        CHUNKS_IN_DIRECTION * CHUNK_SIZE
     );
-    let bytes = CHUNKS_IN_MAP * CHUNKDATA_SIZE;
+    let bytes = CHUNKS_IN_MAP * CHUNK_BYTE_SIZE;
     println!("bytes for a map: {}", bytes);
     println!(
         "terrabits for a map: {:.4}",
@@ -36,19 +26,21 @@ fn calculate_stuff() {
     );
     println!();
     // size of each chunk
-    println!("chunk size: {}", CHUNKSIZE);
-    println!("data size: {}", CHUNKDATA_SIZE);
+    println!("chunk size: {}", CHUNK_SIZE);
+    println!("data size: {}", CHUNK_BYTE_SIZE);
     println!(
         "data size MB: {:.4}",
-        (CHUNKDATA_SIZE) as f64 / 1024.0 / 1024.0
+        (CHUNK_BYTE_SIZE) as f64 / 1024.0 / 1024.0
     );
 
     println!();
     let mut chunk = Chunk::new();
+    let mut one_pixel_chunk = Chunk::new();
+    chunk.set(0, 0, 1);
 
     // data at set indexes
-    for i in 0..CHUNKSIZE {
-        for j in 0..CHUNKSIZE {
+    for i in 0..CHUNK_SIZE {
+        for j in 0..CHUNK_SIZE {
             // random number between 0 and 15
             let value = rand::random::<u8>() % 16;
 
@@ -102,104 +94,4 @@ fn calculate_stuff() {
         "compress size for a map TB: {}",
         (file_bytes as usize * CHUNKS_IN_MAP) as f64 / 1024.0 / 1024.0 / 1024.0 / 1024.0
     );
-
-    // open file, decompress data anc check if it is the same as the original
-    let compressed = std::fs::read("chunk.lz4").unwrap();
-    let decompressed = lz4_flex::decompress(&compressed, CHUNKDATA_SIZE).unwrap();
-
-    let chunk2 = Chunk::from_slice(&decompressed);
-
-    // chunk2.set(11, 1, 1);
-    // check if the data is the same
-    for i in 0..CHUNKINDEX_SIZE {
-        for j in 0..CHUNKINDEX_SIZE {
-            let value = chunk.get(i, j);
-            let value2 = chunk2.get(i, j);
-            // println!("{} {} {} {}", i, j, value, value2);
-            if value != value2 {
-                println!("error at index: {} {} {} {}", i, j, value, value2);
-            }
-        }
-    }
-}
-
-struct Chunk {
-    data: [u8; CHUNKDATA_SIZE],
-}
-
-impl Chunk {
-    fn new() -> Self {
-        Chunk {
-            data: [0; CHUNKDATA_SIZE],
-        }
-    }
-
-    fn from_slice(data: &[u8]) -> Self {
-        let mut packed = Chunk::new();
-        packed.data.copy_from_slice(data);
-        packed
-    }
-
-    fn get(&self, x: usize, y: usize) -> u8 {
-        let index = x + y * CHUNKSIZE;
-        let byte_index = index / 2;
-        let is_high_nibble = index % 2 == 0;
-
-        if is_high_nibble {
-            (self.data[byte_index] & 0xF0) >> 4
-        } else {
-            self.data[byte_index] & 0x0F
-        }
-    }
-
-    fn set(&mut self, x: usize, y: usize, value: u8) {
-        let index = x + y * CHUNKSIZE;
-        let byte_index = index / 2;
-        if byte_index >= CHUNKDATA_SIZE {
-            panic!("index out of bounds");
-        }
-        let is_high_nibble = index % 2 == 0;
-
-        // println!(
-        //     "Before: data[{}] = {:08b}",
-        //     byte_index, self.data[byte_index]
-        // );
-
-        if is_high_nibble {
-            self.data[byte_index] = (self.data[byte_index] & 0x0F) | (value << 4);
-        } else {
-            self.data[byte_index] = (self.data[byte_index] & 0xF0) | (value & 0x0F);
-        }
-
-        // println!(
-        //     "After: data[{}] = {:08b}",
-        //     byte_index, self.data[byte_index]
-        // );
-    }
-}
-
-#[test]
-fn test_packed_u4_array() {
-    let chunk1 = Chunk::new();
-    let mut chunk2 = Chunk::new();
-
-    println!("Before: {:08b}", chunk2.data[0]);
-    chunk2.set(11, 1, 1);
-    println!("After: {:08b}", chunk2.data[0]);
-
-    for i in 0..CHUNKINDEX_SIZE {
-        for j in 0..CHUNKINDEX_SIZE {
-            let value = chunk1.get(i, j);
-            let value2 = chunk2.get(i, j);
-            // println!("{} {} {} {}", i, j, value, value2);
-            if i == 11 && j == 1 {
-                assert_eq!(value2, 1);
-            } else {
-                assert_eq!(value, value2);
-            }
-            if value != value2 {
-                println!("error at index: {} {} {} {}", i, j, value, value2);
-            }
-        }
-    }
 }
